6f5ae798251f4eb3bca2335c4fa6812b42183cda
AgohChris <agihchris90@gmail.com>
2026-04-30 12:51:06 +0000

    Auto-commit du : 30-04-2026 à 12:51:06

--- neogarba/apps/catalogue/models.py
+++ neogarba/apps/catalogue/models.py
@@ -30,4 +30,41 @@ class CategorieMenu(models.Model):
         verbose_name = "Categorie"
         verbose_name_plural = "Categories"
 
+class OptionPersonnalisation(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=generer_uuid7,
+        editable=False
+    )
+
+    reference = models.CharField(
+        max_length=30,
+        unique=True,
+        editable=False
+    )
+
+    type = models.CharField(
+        max_length=30,
+        choices=TypeOptionEnum.choices
+    )
+    prix_supplement = models.PositiveIntegerField(
+        default =0
+    )
+
+    disponible = models.BooleanField(
+        default=True
+    )
+
+    plat = None
+
+    def save(self, *args, **kwargs):
+        if not self.reference:
+            self.reference = generer_reference(PREFIXES['option'])
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Option Personnalisation"
+        verbose_name_plural = "Option Personnalisations"
+
+
 
